bb7762a58dfcbc47590da6eb187ddb621ce7c6d5
saigopin <saigopin@byteridge.com>
2023-08-15 23:23:51 +0530

    added cmments

---
+++ comments_3.ts
@@ -0,0 +1,23 @@
+
+// this is single line commement
+
+// here assiging value to i variable
+// var i = 10;
+
+
+// list 1
+// list 2
+// list 3
+
+// multiline comments
+/**
+ * this is my new code
+ * this will not execute
+ * 
+ * 
+ * 
+ * sas
+ * 
+ * as
+ * asa
+ */
