db254e568b83c960722cf3250126ca204b61821e
saigopin <saigopin@byteridge.com>
2023-08-15 23:17:20 +0530

    added variable and data types code

---
+++ variable_data_types_2.js
@@ -0,0 +1,22 @@
+// data types
+// primitive data types
+// number, string, boolean
+// variables
+// first letter upper or lower case or _
+// Camel Case
+// should not start with numbers
+var firstName = "Sai Gopi";
+var lastName = 'Narimeti';
+var middleName = "Chinnu";
+var city = "Madhira, khammam";
+var myEducationCollegePlace = "Vignan College"; // infer
+var rank = 123222929292929;
+var percentage = 98.23;
+var millage = 23; // infer
+var age = 27;
+console.log("".concat(firstName, " age is ").concat(age));
+// boolean are either true or false
+var areYouSubscribed = true;
+var pass = false;
+var isChecked = false;
+console.log(isChecked);
---
+++ variable_data_types_2.ts
@@ -0,0 +1,35 @@
+// data types
+// primitive data types
+
+// number, string, boolean
+// variables
+
+// first letter upper or lower case or _
+// Camel Case
+// should not start with numbers
+var firstName: string = "Sai Gopi";
+var lastName: string = 'Narimeti';
+var middleName: string = `Chinnu`;
+var city: string = `Madhira, khammam`;
+
+var myEducationCollegePlace = "Vignan College"; // infer
+
+var rank: number = 123222929292929;
+var percentage: number = 98.23;
+var millage = 23; // infer
+var age = 27;
+
+console.log(`${firstName} age is ${age}`);
+
+
+// boolean are either true or false
+
+var areYouSubscribed: boolean = true;
+var pass: boolean = false;
+
+var isChecked = false;
+
+console.log(isChecked);
+
+
+
